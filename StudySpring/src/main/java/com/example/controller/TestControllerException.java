9842a9ff261b0e6ddc85ae4a38d2a772da1463b3
weizheng <929030505@qq.com>
2020-07-01 09:49:26 +0800

    Update TestControllerException.java

--- StudySpring/src/main/java/com/example/controller/TestControllerException.java
+++ StudySpring/src/main/java/com/example/controller/TestControllerException.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
